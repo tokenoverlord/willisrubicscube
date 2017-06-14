38ce7256749d98522461012cb59af0382fe474d8
tokenoverlord <william.chow.256@gmail.com>
2017-06-14 08:57:23 -0700

    Clean up of the solution including some refactoring. The solve cube has not been implemented yet. The solution builds, but still needs work on having the rubics cube rotate reimplemented.

---
+++ WillisRubicsCube/CubieControl.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace WillisRubicsCube
+{
+    partial class CubieControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonCubie = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonCubie
+            // 
+            this.ButtonCubie.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ButtonCubie.Location = new System.Drawing.Point(0, 0);
+            this.ButtonCubie.Name = "ButtonCubie";
+            this.ButtonCubie.Size = new System.Drawing.Size(23, 23);
+            this.ButtonCubie.TabIndex = 56;
+            this.ButtonCubie.UseVisualStyleBackColor = true;
+            // 
+            // CubieControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ButtonCubie);
+            this.Name = "CubieControl";
+            this.Size = new System.Drawing.Size(23, 23);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonCubie;
+    }
+}
---
+++ WillisRubicsCube/CubieControl.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Media3D;
+
+namespace WillisRubicsCube
+{
+    public partial class CubieControl : UserControl
+    {
+        #region Private Members
+        private Cubie _clsCubie;
+        private Point3D _clsLocation = new Point3D();
+        private CubeSurface _flgSurface = CubeSurface.Back;
+        #endregion Private Members
+
+        #region Constructor
+        public CubieControl()
+        {
+            InitializeComponent();
+        }
+        #endregion Constructor
+
+        #region Properties
+        public Point3D Location3D
+        {
+            get { return _clsLocation; }
+            set { _clsLocation = value; }
+        }
+
+        public CubeSurface Surface
+        {
+            get { return _flgSurface; }
+            set { _flgSurface = value; }
+        }
+        #endregion Properties
+
+        #region Public Methods
+        public void LoadCubie(Cubie clsCubie)
+        {
+            this._clsCubie = clsCubie;
+        }
+
+        public override void Refresh()
+        {
+            base.Refresh();
+
+            if (_clsCubie != null)
+            {
+                // get the color
+                Color clsColor = Color.Black;
+
+                // get the color for this surface
+                switch (_flgSurface)
+                {
+                    case CubeSurface.Back:
+                        clsColor = GetColor(_clsCubie.Back);
+                        break;
+                    case CubeSurface.Down:
+                        clsColor = GetColor(_clsCubie.Down);
+                        break;
+                    case CubeSurface.Face:
+                        clsColor = GetColor(_clsCubie.Face);
+                        break;
+                    case CubeSurface.Left:
+                        clsColor = GetColor(_clsCubie.Left);
+                        break;
+                    case CubeSurface.Right:
+                        clsColor = GetColor(_clsCubie.Right);
+                        break;
+                    case CubeSurface.Up:
+                        clsColor = GetColor(_clsCubie.Up);
+                        break;
+                }
+
+                ButtonCubie.BackColor = clsColor;
+            }
+        }
+        #endregion Public Methods
+
+        #region Private Methods
+        private Color GetColor(CubeColor flgColor)
+        {
+            switch (flgColor)
+            {
+                case CubeColor.Blue:
+                    return Color.Blue;
+                case CubeColor.Green:
+                    return Color.Green;
+                case CubeColor.Orange:
+                    return Color.Orange;
+                case CubeColor.Red:
+                    return Color.Red;
+                case CubeColor.White:
+                    return Color.White;
+                case CubeColor.Yellow:
+                    return Color.Yellow;
+                default:
+                    return Color.Black;
+            }
+        }
+        #endregion Private Methods
+    }
+}
